60e6e197241bf08ba37b1293723f4b7af0c534e8
joseph_013 <103237286+Joseph-013@users.noreply.github.com>
2024-01-28 22:33:55 +0800

    Updates
    Fixed admin dashboard routing
    Edit Readme

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::redirect('/', '/login');
+Route::redirect('/', 'login');
+
+// Route::view('dashboard', 'dashboard')
+//     ->middleware(['auth', 'verified'])
+//     ->name('dashboard');
 
 Route::view('/admin', 'livewire/main/admin/inventory')
     ->middleware(['auth', 'verified'])

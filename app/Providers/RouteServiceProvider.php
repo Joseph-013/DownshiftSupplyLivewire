21e71d9afa0ae92706e90fbbde0a6462feaab02f
joseph_013 <103237286+Joseph-013@users.noreply.github.com>
2024-02-03 18:25:23 +0800

    inventory UI working

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/user/products';
+    public const HOME = '/admin/inventory';
 
     /**
      * Define your route model bindings, pattern filters, and other route configuration.

dee7d36fa7e8cd906112e0e5d4965e9d0a9fae26
jamessuuu <jameslorenzsantos@gmail.com>
2024-02-11 12:42:22 +0800

    Update usernav.blade.php

--- resources/views/livewire/layout/usernav.blade.php
+++ resources/views/livewire/layout/usernav.blade.php
@@ -1,15 +1,17 @@
-<div class="hidden space-x-8 sm:-my-px sm:ms-5 lg:ms-8 md:flex">
-    <x-nav-link :href="route('user.products')" :active="request()->routeIs('products')" wire:navigate>
-        Products
-    </x-nav-link>
-</div>
-<div class="hidden space-x-8 sm:-my-px sm:ms-5 lg:ms-8 md:flex">
-    <x-nav-link :href="route('user.orders')" :active="request()->routeIs('orders')" wire:navigate>
-        Orders
-    </x-nav-link>
-</div>
-<div class="hidden space-x-8 sm:-my-px sm:ms-5 lg:ms-8 md:flex">
-    <x-nav-link :href="route('user.faqs')" :active="request()->routeIs('faqs')" wire:navigate>
-        FAQs
-    </x-nav-link>
+<div class="flex justify-end ml-20 gap-10 pl-20"> <!-- Adjusted alignment -->
+    <div class="hidden space-x-8 sm:-my-px sm:ms-5 lg:ms-8 md:flex">
+        <x-nav-link :href="route('user.products')" :active="request()->routeIs('products')" wire:navigate>
+            Products
+        </x-nav-link>
+    </div>
+    <div class="hidden space-x-8 sm:-my-px sm:ms-5 lg:ms-8 md:flex">
+        <x-nav-link :href="route('user.orders')" :active="request()->routeIs('orders')" wire:navigate>
+            Orders
+        </x-nav-link>
+    </div>
+    <div class="hidden space-x-8 sm:-my-px sm:ms-5 lg:ms-8 md:flex">
+        <x-nav-link :href="route('user.faqs')" :active="request()->routeIs('faqs')" wire:navigate>
+            FAQs
+        </x-nav-link>
+    </div>
 </div>

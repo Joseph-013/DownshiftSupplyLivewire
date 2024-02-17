0a8e651e7162117f79f282ed2c730d8d018a2a47
joseph_013 <103237286+Joseph-013@users.noreply.github.com>
2024-02-17 23:49:55 +0800

    cart and model update

--- app/Livewire/Main/User/Livewire/UserProducts.php
+++ app/Livewire/Main/User/Livewire/UserProducts.php
@@ -2,16 +2,34 @@
 
 namespace App\Livewire\Main\User\Livewire;
 
-use Livewire\Component;
+use App\Models\Cart;
 use App\Models\Product;
+use Livewire\Component;
+use Illuminate\Support\Facades\Auth;
 
 class UserProducts extends Component
 {
 
     public $products;
 
-    public function addToCart($productId) {
-        dd("$productId is added to cart");
+    public function addToCart($productId)
+    {
+        $userId = Auth::id();
+        $productSelected = Product::findOrFail($productId);
+        $existingCartEntry = Cart::where('user_id', $userId)
+            ->where('product_id', $productId)
+            ->exists();
+
+        if (!$existingCartEntry) {
+            $result = Cart::create([
+                'user_id' => $userId,
+                'product_id' => $productId,
+                'subtotal' => $productSelected->price,
+            ]);
+            dump("Userid $result->user_id has added productid $result->product_id to cart");
+        } else {
+            dump("$productId already exists in cart");
+        }
     }
 
     public function render()

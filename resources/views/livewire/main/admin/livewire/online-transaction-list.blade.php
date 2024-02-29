1f97dc1cf403235d51d47c36a715435d7e710a7e
Qiroo <valdecanasjustin@gmail.com>
2024-02-29 22:53:49 +0800

    .

--- resources/views/livewire/main/admin/livewire/online-transaction-list.blade.php
+++ resources/views/livewire/main/admin/livewire/online-transaction-list.blade.php
@@ -4,7 +4,7 @@
         @if($transaction->purchaseType === "Online")
         <li wire:click="selectTransaction({{ $transaction->id }})" class="w-full flex justify-center select-none px-2">
             {{-- Product Details --}}
-            <input @if($transaction->id) class="widenWhenSelected" @endif hidden type="radio" id="transactionId{{ $transaction->id }}" name="productList">
+            <input class="widenWhenSelected" hidden type="radio" id="transactionId{{ $transaction->id }}" name="productList">
             <label class="w-11/12 py-2 my-1 rounded-full border-2 border-gray shadow-sm text-sm flex items-center" for="transactionId{{ $transaction->id }}">
                 <ul class="flex flex-row w-full">
                     <li class="w-2/12 text-center text-xs">{{ $transaction->id }}</li>

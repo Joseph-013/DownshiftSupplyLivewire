578279d875d41e017e129eca773a16aa93aecbbb
joseph_013 <103237286+Joseph-013@users.noreply.github.com>
2024-03-05 10:17:41 +0800

    nav bar fixes

--- resources/views/livewire/layout/adminnav.blade.php
+++ resources/views/livewire/layout/adminnav.blade.php
@@ -1,25 +1,28 @@
-<div class="hidden space-x-8 sm:-my-px sm:ms-5 lg:ms-8 md:flex ml-20 gap-10 pl-20">
-    <x-nav-link :href="route('admin.inventory')" :active="request()->routeIs('admin.inventory')" wire:navigate>
-        Inventory
-    </x-nav-link>
-</div>
-<div class="hidden space-x-8 sm:-my-px sm:ms-3 lg:ms-8 md:flex">
-    <x-nav-link :href="route('admin.onsitetransactions')" :active="request()->routeIs('admin.onsitetransactions')" wire:navigate>
-        Onsite<br />Transactions
-    </x-nav-link>
-</div>
-<div class="hidden space-x-8 sm:-my-px sm:ms-3 lg:ms-8 md:flex">
-    <x-nav-link :href="route('admin.onlinetransactions')" :active="request()->routeIs('admin.onlinetransactions')" wire:navigate>
-        Online<br />Transactions
-    </x-nav-link>
-</div>
-<div class="hidden space-x-8 sm:-my-px sm:ms-3 lg:ms-8 md:flex">
-    <x-nav-link :href="route('admin.faqs')" :active="request()->routeIs('admin.faqs')" wire:navigate>
-        FAQs
-    </x-nav-link>
-</div>
-<div class="hidden space-x-8 sm:-my-px sm:ms-3 lg:ms-8 md:flex">
-    <x-nav-link :href="route('admin.reports')" :active="request()->routeIs('admin.reports')" wire:navigate>
-        Reports
-    </x-nav-link>
+<div class="flex h-full">
+    <div class="hidden space-x-8 sm:-my-px sm:ms-2 lg:ms-8 md:flex gap-10">
+        <x-nav-link :href="route('admin.inventory')" :active="request()->routeIs('admin.inventory')" wire:navigate>
+            Inventory
+        </x-nav-link>
+    </div>
+    <div class="hidden space-x-8 sm:-my-px sm:ms-2 lg:ms-8 md:flex">
+        <x-nav-link :href="route('admin.onsitetransactions')" :active="request()->routeIs('admin.onsitetransactions')" wire:navigate>
+            Onsite<br />Transactions
+        </x-nav-link>
+    </div>
+    <div class="hidden space-x-8 sm:-my-px sm:ms-2 lg:ms-8 md:flex">
+        <x-nav-link :href="route('admin.onlinetransactions')" :active="request()->routeIs('admin.onlinetransactions')" wire:navigate>
+            Online<br />Transactions
+        </x-nav-link>
+    </div>
+    <div class="hidden space-x-8 sm:-my-px sm:ms-2 lg:ms-8 md:flex">
+        <x-nav-link :href="route('admin.faqs')" :active="request()->routeIs('admin.faqs')" wire:navigate>
+            FAQs
+        </x-nav-link>
+    </div>
+    <div class="hidden space-x-8 sm:-my-px sm:ms-2 lg:ms-8 md:flex">
+        <x-nav-link :href="route('admin.reports')" :active="request()->routeIs('admin.reports')" wire:navigate>
+            Reports
+        </x-nav-link>
+    </div>
+
 </div>
--- resources/views/livewire/layout/navigation.blade.php
+++ resources/views/livewire/layout/navigation.blade.php
@@ -33,40 +33,47 @@ new class extends Component {
                         </h1>
                     </a>
                 </div>
-
-                <!-- Navigation Links (My stupid code lmao) -->
-                @if (auth()->check())
-                    <!-- Content for authenticated users -->
-                    @auth
-                        @if (auth()->user()->usertype === 'admin')
-                            @include('livewire.layout.adminnav')
-                        @else
-                            @include('livewire.layout.usernav')
-                        @endif
-                    @endauth
-                @else
-                    <!-- Content for guests -->
-                    @include('livewire.layout.usernav')
-                @endif
-
                 <!-- /Navigation Links -->
             </div>
+
+            @if (auth()->check())
+                <!-- Content for authenticated users -->
+                @auth
+                    @if (auth()->user()->usertype === 'admin')
+                        @include('livewire.layout.adminnav')
+                    @else
+                        @include('livewire.layout.usernav')
+                    @endif
+                @endauth
+            @else
+                <!-- Content for guests -->
+                @include('livewire.layout.usernav')
+            @endif
+            <!-- Navigation Links (My stupid code lmao) -->
+
 
             <!-- Settings Dropdown -->
             <div class="h-full flex items-center">
 
-                @if (!auth()->check() || auth()->user()->usertype === 'user')
-                    <a href="{{ route('user.cart') }}" class="h-8 w-8">
-                        <img class="" src="{{ asset('assets/cart.png') }}" alt="cart" />
-                    </a>
-                @endif
+                <div class="h-8 w-8 me-6">
+                    @if (!auth()->check() || auth()->user()->usertype === 'user')
+                        <a href="{{ route('user.cart') }}" class="h-full w-full">
+                            <img class="" src="{{ asset('assets/cart.png') }}" alt="cart" />
+                        </a>
+                    @elseif (!auth()->check() || auth()->user()->usertype === 'admin')
+                        <div class="h-full flex items-center italic text-sm font-bold text-orange-500">
+                            ADMIN
+                        </div>
+                    @endif
+
+                </div>
 
 
-                <div class="hidden md:flex sm:items-center sm:ms-6">
+                <div class="hidden md:flex sm:items-center">
                     <x-dropdown align="right" width="48">
                         <x-slot name="trigger">
                             <button
-                                class="inline-flex items-center px-3 py-2 text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150">
+                                class="inline-flex items-center px-2 lg:px-3 py-2 text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150">
                                 <div>
                                     <img src="{{ asset('assets/profile.jpg') }}" alt="Logo" class="block h-5 w-5">
                                 </div>
@@ -93,34 +100,47 @@ new class extends Component {
                         </x-slot>
                     </x-dropdown>
                 </div>
+
+                <!-- Hamburger -->
+                <div class="-me-2 flex items-center md:hidden">
+                    <button @click="open = ! open"
+                        class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out">
+                        <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
+                            <path :class="{ 'hidden': open, 'inline-flex': !open }" class="inline-flex"
+                                stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="M4 6h16M4 12h16M4 18h16" />
+                            <path :class="{ 'hidden': !open, 'inline-flex': open }" class="hidden"
+                                stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="M6 18L18 6M6 6l12 12" />
+                        </svg>
+                    </button>
+                </div>
             </div>
 
 
 
-            <!-- Hamburger -->
-            <div class="-me-2 flex items-center md:hidden">
-                <button @click="open = ! open"
-                    class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out">
-                    <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
-                        <path :class="{ 'hidden': open, 'inline-flex': !open }" class="inline-flex"
-                            stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                            d="M4 6h16M4 12h16M4 18h16" />
-                        <path :class="{ 'hidden': !open, 'inline-flex': open }" class="hidden" stroke-linecap="round"
-                            stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
-                    </svg>
-                </button>
-            </div>
         </div>
     </div>
 
     <!-- Responsive Navigation Menu -->
     <div :class="{ 'block': open, 'hidden': !open }" class="hidden md:hidden">
         <div class="pt-2 pb-3 space-y-1">
-            @include('livewire.layout.adminnavresponsive')
+            @if (auth()->check())
+                @auth
+                    @if (auth()->user()->usertype === 'admin')
+                        @include('livewire.layout.adminnavresponsive')
+                    @else
+                        @include('livewire.layout.usernavresponsive')
+                    @endif
+                @endauth
+            @else
+                <!-- Content for guests -->
+                @include('livewire.layout.usernav')
+            @endif
         </div>
 
         <!-- Responsive Settings Options -->
-        <div class="pt-4 pb-1 border-t border-gray-200">
+        <div class="pt-4 pb-1 border-t border-gray-200 border border-b-2">
             <div class="px-4">
                 <div class="font-medium text-base text-gray-800 mb-2">
                     <img src="{{ asset('assets/profile.jpg') }}" alt="Logo" class="block h-7 w-7 mr-4">

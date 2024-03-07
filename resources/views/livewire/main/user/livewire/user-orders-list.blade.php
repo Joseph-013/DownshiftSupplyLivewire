e8170fe49712bc799bff203409027ba59db6e004
joseph_013 <103237286+Joseph-013@users.noreply.github.com>
2024-03-07 13:48:06 +0800

    Update user-orders-list.blade.php

--- resources/views/livewire/main/user/livewire/user-orders-list.blade.php
+++ resources/views/livewire/main/user/livewire/user-orders-list.blade.php
@@ -21,47 +21,47 @@
                         {{-- <livewire:main.user.livewire.user-orders-detail-compact orderId="{{ $transaction->id }}"
                             wire:key="{{ $transaction->id }}" /> --}}
                         <li wire:key="{{ $transaction->id }}" class="w-full flex justify-center select-none px-2">
-                            <button @click="open = ! open" class="w-full h-full">
+                            {{-- <button @click="open = ! open" class="w-full h-full"> --}}
 
-                                @if ($transaction->id == $selectedOrder && $windowWidth < 1024)
-                                    <div class="w-full">
-                                        {{ $transaction->id }}
-                                        {{ $transaction->paymentOption }}
-                                        {{ $transaction->shippingAddress }}
+                            @if ($transaction->id == $selectedOrder && $windowWidth < 1024)
+                                <div class="w-full">
+                                    {{ $transaction->id }}
+                                    {{ $transaction->paymentOption }}
+                                    {{ $transaction->shippingAddress }}
+                                    {{ $transaction->purchaseDate->format('Y-m-d') }}
+                                    {{ $transaction->preferredService }}
+                                    {{ $transaction->shippingAddress }}
+                                    {{ $transaction->courierUsed }}
+                                    {{ $transaction->trackingNumber }}
+                                    @foreach ($orderList as $order)
+                                        {{ $order->products->name }}
+                                        {{ number_format($order->products->price, 2) }}
+                                        {{ $order->quantity }}
+                                        {{ $order->quantity * $order->products->price }}
+                                    @endforeach
+                                    {{-- style here --}}
+                                </div>
+                            @endif
+
+                            <input class="widenWhenSelected" hidden type="radio" id="productId{{ $transaction->id }}"
+                                name="productList">
+                            <label wire:click="showDetails({{ $transaction->id }})"
+                                class="w-11/12 py-2 my-1 rounded-full btransaction-2 btransaction-gray shadow-sm text-sm items-center"
+                                {{-- for="productId{{ $transaction->id }}" :class="{ 'flex': open, 'hidden': !open }"> --}} for="productId{{ $transaction->id }}">
+                                <ul class="flex flex-row w-full">
+                                    <li class="w-2/12 text-center text-sm">{{ $transaction->id }}</li>
+                                    <li class="w-2/12 text-center text-sm">
                                         {{ $transaction->purchaseDate->format('Y-m-d') }}
+                                    </li>
+                                    <li class="w-3/12 text-center text-sm">
                                         {{ $transaction->preferredService }}
-                                        {{ $transaction->shippingAddress }}
-                                        {{ $transaction->courierUsed }}
-                                        {{ $transaction->trackingNumber }}
-                                        @foreach ($orderList as $order)
-                                            {{ $order->products->name }}
-                                            {{ number_format($order->products->price, 2) }}
-                                            {{ $order->quantity }}
-                                            {{ $order->quantity * $order->products->price }}
-                                        @endforeach
-                                        {{-- style here --}}
-                                    </div>
-                                @endif
-
-                                <input class="widenWhenSelected" hidden type="radio"
-                                    id="productId{{ $transaction->id }}" name="productList">
-                                <label wire:click="showDetails({{ $transaction->id }})"
-                                    class="w-11/12 py-2 my-1 rounded-full btransaction-2 btransaction-gray shadow-sm text-sm items-center"
-                                    for="productId{{ $transaction->id }}" :class="{ 'flex': open, 'hidden': !open }">
-                                    <ul class="flex flex-row w-full">
-                                        <li class="w-2/12 text-center text-sm">{{ $transaction->id }}</li>
-                                        <li class="w-2/12 text-center text-sm">
-                                            {{ $transaction->purchaseDate->format('Y-m-d') }}
-                                        </li>
-                                        <li class="w-3/12 text-center text-sm">
-                                            {{ $transaction->preferredService }}
-                                        <li class="w-2/12 text-center text-sm">{{ $transaction->status }}</li>
-                                        <li class="w-2/12 text-center text-sm">
-                                            {{ number_format($transaction->grandTotal, 2) }}
-                                        </li>
-                                    </ul>
-                                </label>
-                            </button>
+                                    <li class="w-2/12 text-center text-sm">{{ $transaction->status }}</li>
+                                    <li class="w-2/12 text-center text-sm">
+                                        {{ number_format($transaction->grandTotal, 2) }}
+                                    </li>
+                                </ul>
+                            </label>
+                            {{-- </button> --}}
 
                         </li>
                     @endforeach

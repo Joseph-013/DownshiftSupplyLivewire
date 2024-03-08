5456ed41e749e2edc02328139002ea2d90a6d14a
joseph_013 <103237286+Joseph-013@users.noreply.github.com>
2024-03-08 21:36:39 +0800

    Update user-cart.blade.php

--- resources/views/livewire/main/user/livewire/user-cart.blade.php
+++ resources/views/livewire/main/user/livewire/user-cart.blade.php
@@ -37,7 +37,7 @@
                                 data-stock-quantity="{{ $entry->product->stockquantity }}"
                                 aria-describedby="helper-text-explanation"
                                 class="bg-white border-gray-300 h-8 text-center text-gray-900 text-sm focus:ring-blue-500 focus:border-blue-500 block w-full py-1.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                value='{{ $entry->quantity }}' required>
+                                value='{{ $entry->quantity }}' required max="{{ $entry->product->stockquantity }}">
                             <button wire:click="incrementQuantity({{ $entry->product->id }})" type="button"
                                 id="increment-button" data-input-counter-increment="quantity-input"
                                 class="bg-white dark:bg-gray-700 dark:hover:bg-gray-600 dark:border-gray-600 hover:bg-gray-200 border border-gray-300 rounded-sm p-2 h-8 focus:ring-gray-100 dark:focus:ring-gray-700 focus:ring-2 focus:outline-none">
@@ -49,7 +49,7 @@
                             </button>
                         </div>
                         <div class="remaining-text text-left text-red-500 italic text-xs mt-1">
-                            {{ $entry->product->stockquantity <= $entry->product->criticallevel ? $entry->product->stockquantity . ' item/s left' : '' }}
+                            {{ $entry->product->stockquantity <= $entry->product->criticallevel || $entry->quantity >= $entry->product->stockquantity ? $entry->product->stockquantity . ' item/s left' : '' }}
                         </div>
                     </div>
                 </li>

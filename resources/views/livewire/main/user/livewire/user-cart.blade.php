33ef0498efb8ae129b175db335b2e3d91b9825e6
jamessuuu <jameslorenzsantos@gmail.com>
2024-03-08 18:51:25 +0800

    Update user-cart.blade.php

--- resources/views/livewire/main/user/livewire/user-cart.blade.php
+++ resources/views/livewire/main/user/livewire/user-cart.blade.php
@@ -11,7 +11,6 @@
                 <div class="container">
                     <div class="fixed-width-container text-left px-0">
                     <span class="flex-grow text-left">{{ Str::limit($entry->product->name, 40) }}</span>
-
                         <div class="remaining-text text-left text-red-500 italic text-xs">XX remaining left</div>
 
                     </div>

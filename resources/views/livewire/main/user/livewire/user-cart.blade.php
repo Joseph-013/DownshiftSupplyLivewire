313d6daf6d7c0ffff5a6af7db5e84653c8ec2312
jamessuuu <jameslorenzsantos@gmail.com>
2024-03-08 21:34:21 +0800

    Update user-cart.blade.php

--- resources/views/livewire/main/user/livewire/user-cart.blade.php
+++ resources/views/livewire/main/user/livewire/user-cart.blade.php
@@ -58,5 +58,16 @@
 
     </li>
     @endforeach
-
+    <style>
+    /* Remove increment and decrement arrows */
+    input[type="number"]::-webkit-inner-spin-button,
+    input[type="number"]::-webkit-outer-spin-button {
+        -webkit-appearance: none;
+        margin: 0;
+    }
+    input[type="number"] {
+        -moz-appearance: textfield;
+    }
+</style>
 </ul>
+

a4cefcc63f14db1a60ab84d962369a71a26d95a0
jamessuuu <jameslorenzsantos@gmail.com>
2024-03-20 22:10:11 +0800

    table done

--- resources/views/livewire/main/admin/livewire/product-list.blade.php
+++ resources/views/livewire/main/admin/livewire/product-list.blade.php
@@ -1,33 +1,35 @@
 <div class="w-full h-full">
     <div class="w-full h-96 overflow-y-auto" id="questions-container">
-        <ul class="w-full flex flex-col items-center">
-            @if ($products)
-                @foreach ($products as $product)
-                    {{-- Single Unit of Product --}}
-                    <li wire:key="{{ $product->id }}" wire:click="selectProduct({{ $product->id }})"
-                        class="w-full flex justify-center select-none px-2">
-                        {{-- Product Details --}}
-                        <input class="widenWhenSelected" hidden type="radio" id="productId{{ $product->id }}"
-                            name="productList">
-                        <label
-                            class="
-                @if ($product->stockquantity <= 0) {{ 'bg-gray-400 ' }}
-                @elseif ($product->stockquantity <= $product->criticallevel) {{ 'bg-red-400 ' }} @endif
-                w-11/12 py-2 my-1 rounded-full border-2 border-gray shadow-sm text-sm flex items-center {{ $product->stockquantity <= $product->criticallevel ? 'text-white' : '' }}"
-                            for="productId{{ $product->id }}">
-                            <ul class="flex flex-row w-full">
-                                <li class="w-1/12 pl-3 -mr-2 text-left text-sm">{{ $product->id }}</li>
-                                <li class="w-7/12 px-3 text-left text-sm">{{ $product->name }}</li>
-                                <li class="w-1/12 pl-6 text-center text-sm">{{ $product->stockquantity }}</li>
-                                <li class="w-3/12 text-center text-sm">₱&nbsp;{{ number_format($product->price, 2) }}
-                                </li>
-                            </ul>
-                        </label>
-                    </li>
-                @endforeach
-            @endif
+    <table class="w-full">
+    <tbody>
+        @if ($products)
+            @foreach ($products as $product)
+            <tr class="w-full">
+                <td class="w-full flex justify-center select-none px-2">
+                    <input class="widenWhenSelected" hidden type="radio" id="productId{{ $product->id }}"
+                        name="productList">
+                    <label
+                        class="
+                        @if ($product->stockquantity <= 0) {{ 'bg-gray-400 ' }}
+                        @elseif ($product->stockquantity <= $product->criticallevel) {{ 'bg-red-400 ' }} @endif
+                        w-full py-2 my-1 rounded-full border-2 border-gray shadow-sm text-sm flex items-center {{ $product->stockquantity <= $product->criticallevel ? 'text-white' : '' }}"
+                        for="productId{{ $product->id }}">
+                        <table class="w-full">
+                            <tr class="flex flex-row w-full">
+                                <td class="w-1/12 pl-3 -mr-2 text-left text-sm">{{ $product->id }}</td>
+                                <td class="w-7/12 px-3 text-left text-sm">{{ $product->name }}</td>
+                                <td class="w-1/12 pl-6 text-left text-sm">{{ $product->stockquantity }}</td>
+                                <td class="w-3/12 text-center text-sm">₱&nbsp;{{ number_format($product->price, 2) }}</td>
+                            </tr>
+                        </table>
+                    </label>
+                </td>
+            </tr>
+            @endforeach
+        @endif
+    </tbody>
+</table>
 
-        </ul>
     </div>
 
     <div class="flex flex-row items-center mt-4">

fb0b84f2fe68012d166bd98c0366673018aef110
jamessuuu <jameslorenzsantos@gmail.com>
2024-03-21 22:39:59 +0800

    Update user-products.blade.php

--- resources/views/livewire/main/user/livewire/user-products.blade.php
+++ resources/views/livewire/main/user/livewire/user-products.blade.php
@@ -1,10 +1,62 @@
 <div>
-    <ul class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-3 gap-y-16 w-full overflow-y-auto"
-        id="questions-container">
+<div class="absolute top-0 left-0 w-full h-full flex items-center justify-center border">
+    <!-- Semi-transparent overlay -->
+    <div class="absolute inset-0 bg-black opacity-50"></div>
+
+    <!-- Product details card -->
+    <div class="bg-gray-100 p-6 rounded-lg relative z-50 border flex flex-col items-center justify-center">
+        <div class="mb-2">
+            <img class="rounded-md object-cover" src="{{ isset($selectedProduct->image) ? (filter_var($selectedProduct->image, FILTER_VALIDATE_URL) ? $selectedProduct->image : asset('storage/assets/' . $selectedProduct->image)) : 'https://via.placeholder.com/350x200.png/000000?text=...' }}" alt="{{ isset($selectedProduct->image) ? $selectedProduct->name : 'No Image Selected' }}" style="max-height: 200px;">
+        </div>
+        <table class="w-full">
+            <tr class="h-14">
+                <th colspan="2" class="text-center font-semibold">Product Details</th>
+            </tr>
+            <tr class="h-9">
+                <td class="w-1/3 text-left text-gray-500">Name:</td>
+                <td class="w-2/3 text-left">
+                    @php
+                    $name = "Nameeeee";
+                    $nameChunks = str_split($name, 20); // Split name into chunks of 20 characters
+                    @endphp
+                    @foreach($nameChunks as $chunk)
+                    {{ $chunk }}<br> <!-- Add a line break after each 20 characters -->
+                    @endforeach
+                </td>
+            </tr>
+            <tr class="h-9">
+                <td class="w-1/3 text-left text-gray-500">Description:</td>
+                <td class="w-2/3 text-left">
+                    @php
+                    $description = "Lorem ipsum dolor sit asdadasdasdasdasdadsa";
+                    $descriptionChunks = str_split($description, 20); // Split description into chunks of 20 characters
+                    @endphp
+                    @foreach($descriptionChunks as $chunk)
+                    {{ $chunk }}<br>
+                    @endforeach
+                </td>
+            </tr>
+            <tr class="h-9">
+                <td class="w-1/3 text-left text-gray-500">Price:</td>
+                <td class="w-2/3 text-left">
+                    P 50,000.00
+                </td>
+            </tr>
+            <tr class="h-9">
+                <td class="w-1/3 text-left text-gray-500">Stock:</td>
+                <td class="w-2/3 text-left">asdasdad</td>
+            </tr>
+        </table>
+    </div>
+</div>
+
+
+
+    <ul class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-3 gap-y-16 w-full overflow-y-auto" id="questions-container">
 
         {{-- Separate Overlay to New Component and use isset --}}
         {{-- <div id="overlay"
-            class="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 flex justify-center items-center z-50">
+            class="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 flex justify-center items-center z-20">
             <div class="bg-white p-6 rounded-lg shadow-lg">
                 <h2 class="text-xl mb-4 text-center">Have you received order XXXXXXXXloveu yet?</h2>
                 <p class="text-sm text-gray-600 mb-4 text-center">Once you choose yes, the order is set to complete. This
@@ -24,61 +76,52 @@
         </div> --}}
 
         @if ($products)
-            @foreach ($products as $product)
-                <li class="px-2 text-left text-sm relative">
-                    <div class="w-full h-5/6 relative">
-                        <img src="{{ filter_var($product->image, FILTER_VALIDATE_URL) ? $product->image : asset('storage/assets/' . $product->image) }}"
-                            class="w-full h-full rounded-lg border-gray-500 border-1 object-fit">
-                        @if ($product->stockquantity <= 0)
-                            <div class="absolute inset-0 flex justify-center items-center">
-                                <div
-                                    class="bg-black bg-opacity-50 text-white text-lg font-bold py-2 px-4 rounded-lg max-w-full absolute inset-0 flex justify-center items-center">
-                                    Out of Stock
-                                </div>
-                            </div>
-                        @endif
+        @foreach ($products as $product)
+        <li class="px-2 text-left text-sm relative">
+            <div class="w-full h-5/6 relative">
+                <img src="{{ filter_var($product->image, FILTER_VALIDATE_URL) ? $product->image : asset('storage/assets/' . $product->image) }}" class="w-full h-full rounded-lg border-gray-500 border-1 object-fit">
+                @if ($product->stockquantity <= 0) <div class="absolute inset-0 flex justify-center items-center">
+                    <div class="bg-black bg-opacity-50 text-white text-lg font-bold py-2 px-4 rounded-lg max-w-full absolute inset-0 flex justify-center items-center">
+                        Out of Stock
                     </div>
+            </div>
+            @endif
+</div>
 
-                    <div class="w-full h-10 flex items-center">
-                        <ul class="flex flex-row w-full items-center">
-                            <li class="w-10/12 pl-2 text-left text-sm">
-                                <div class="text-left text-sm mt-4 leading-5 line-clamp-2">
-                                    {{ $product->name }}
-                                </div>
-                                <div class="mt-2 text-left text-sm mb-[-1rem]">
-                                    ₱&nbsp;{{ number_format($product->price, 2) }}
-                                </div>
-                            </li>
-                            <li class="w-2/12 text-left text-sm flex justify-center">
-                                @if ($product->stockquantity > 0)
-                                    <button wire:click="addToCart({{ $product->id }})">
-                                        <svg class="svg-icon mt-3 w-full h-full hover:brightness-90"
-                                            style="width: 2.5em; height: 2.5em; vertical-align: middle; fill: #fb923c; overflow: hidden;"
-                                            viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg">
-                                            <path
-                                                d="M512 96C282.2 96 96 282.2 96 512s186.2 416 416 416 416-186.2 416-416S741.8 96 512 96z m181 448H544v149c0 17.6-14.4 32-32 32-8.8 0-16.8-3.6-22.6-9.4-5.8-5.8-9.4-13.8-9.4-22.6V544h-149c-8.8 0-16.8-3.6-22.6-9.4-5.8-5.8-9.4-13.8-9.4-22.6 0-17.6 14.4-32 32-32H480v-149c0-17.6 14.4-32 32-32s32 14.4 32 32V480h149c17.6 0 32 14.4 32 32s-14.4 32-32 32z" />
-                                        </svg>
-                                    </button>
-                                @else
-                                    <button disabled>
-                                        <svg class="svg-icon mt-3 w-full h-full hover:brightness-90"
-                                            style="width: 2.5em; height: 2.5em; vertical-align: middle; fill: #ccc; overflow: hidden;"
-                                            viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg">
-                                            <path
-                                                d="M512 96C282.2 96 96 282.2 96 512s186.2 416 416 416 416-186.2 416-416S741.8 96 512 96z m181 448H544v149c0 17.6-14.4 32-32 32-8.8 0-16.8-3.6-22.6-9.4-5.8-5.8-9.4-13.8-9.4-22.6V544h-149c-8.8 0-16.8-3.6-22.6-9.4-5.8-5.8-9.4-13.8-9.4-22.6 0-17.6 14.4-32 32-32H480v-149c0-17.6 14.4-32 32-32s32 14.4 32 32V480h149c17.6 0 32 14.4 32 32s-14.4 32-32 32z" />
-                                        </svg>
-                                    </button>
-                                @endif
-                            </li>
-                        </ul>
-                    </div>
-                </li>
-            @endforeach
-        @endif
+<div class="w-full h-10 flex items-center">
+    <ul class="flex flex-row w-full items-center">
+        <li class="w-10/12 pl-2 text-left text-sm">
+            <div class="text-left text-sm mt-4 leading-5 line-clamp-2">
+                {{ $product->name }}
+            </div>
+            <div class="mt-2 text-left text-sm mb-[-1rem]">
+                ₱&nbsp;{{ number_format($product->price, 2) }}
+            </div>
+        </li>
+        <li class="w-2/12 text-left text-sm flex justify-center">
+            @if ($product->stockquantity > 0)
+            <button wire:click="addToCart({{ $product->id }})">
+                <svg class="svg-icon mt-3 w-full h-full hover:brightness-90" style="width: 2.5em; height: 2.5em; vertical-align: middle; fill: #fb923c; overflow: hidden;" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M512 96C282.2 96 96 282.2 96 512s186.2 416 416 416 416-186.2 416-416S741.8 96 512 96z m181 448H544v149c0 17.6-14.4 32-32 32-8.8 0-16.8-3.6-22.6-9.4-5.8-5.8-9.4-13.8-9.4-22.6V544h-149c-8.8 0-16.8-3.6-22.6-9.4-5.8-5.8-9.4-13.8-9.4-22.6 0-17.6 14.4-32 32-32H480v-149c0-17.6 14.4-32 32-32s32 14.4 32 32V480h149c17.6 0 32 14.4 32 32s-14.4 32-32 32z" />
+                </svg>
+            </button>
+            @else
+            <button disabled>
+                <svg class="svg-icon mt-3 w-full h-full hover:brightness-90" style="width: 2.5em; height: 2.5em; vertical-align: middle; fill: #ccc; overflow: hidden;" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M512 96C282.2 96 96 282.2 96 512s186.2 416 416 416 416-186.2 416-416S741.8 96 512 96z m181 448H544v149c0 17.6-14.4 32-32 32-8.8 0-16.8-3.6-22.6-9.4-5.8-5.8-9.4-13.8-9.4-22.6V544h-149c-8.8 0-16.8-3.6-22.6-9.4-5.8-5.8-9.4-13.8-9.4-22.6 0-17.6 14.4-32 32-32H480v-149c0-17.6 14.4-32 32-32s32 14.4 32 32V480h149c17.6 0 32 14.4 32 32s-14.4 32-32 32z" />
+                </svg>
+            </button>
+            @endif
+        </li>
     </ul>
-    <div class="flex flex-row items-center mt-4 justify-center w-full">
-        <div class="w-96 text-center px-3">
-            {{ $products->links('pagination.default') }}
-        </div>
+</div>
+</li>
+@endforeach
+@endif
+</ul>
+<div class="flex flex-row items-center mt-4 justify-center w-full">
+    <div class="w-96 text-center px-3">
+        {{ $products->links('pagination.default') }}
     </div>
+</div>
 </div>

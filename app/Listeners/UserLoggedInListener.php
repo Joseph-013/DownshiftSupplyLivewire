bc6ae3fab708b20d57c2f9e97f4c868a2c2777b3
joseph_013 <103237286+Joseph-013@users.noreply.github.com>
2024-03-26 12:33:53 +0800

    listener finished

---
+++ app/Listeners/UserLoggedInListener.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Models\Transaction;
+use Carbon\Carbon;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Auth\Events\Authenticated;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class UserLoggedInListener
+{
+    /**
+     * Create the event listener.
+     */
+    // public function __construct()
+    // {
+    //     //
+    // }
+
+    /**
+     * Handle the event.
+     */
+    public function handle(Authenticated $event)
+    {
+        $user = $event->user;
+
+        if ($user->usertype = 'user') {
+
+            $inTransitTransactions = Transaction::where('user_id', $user->id)->where('status', 'In Transit')->get();
+            // dd($inTransitTransactions);
+
+            $modifiedTransactions = [];
+
+            foreach ($inTransitTransactions as $transaction) {
+                if (Carbon::now()->diffInDays($transaction->intransit_at) >= 7) {
+                    $transaction->status = 'Complete';
+                    $transaction->save();
+                    $modifiedTransactions[] = $transaction->id;
+                }
+            }
+
+            if (!empty($modifiedTransactions)) {
+                session()->flash('complete', $modifiedTransactions);
+            }
+        }
+    }
+}

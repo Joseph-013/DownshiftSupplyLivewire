09a02d7c0dc22ade002ef9a4ebea1e730f590616
joseph_013 <103237286+Joseph-013@users.noreply.github.com>
2024-04-04 23:15:24 +0800

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,22 +13,22 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call(UsersTableSeeder::class);
-        // \App\Models\User::factory(18)->create();
+        \App\Models\User::factory(18)->create();
 
-        $this->call(ProductsTableSeeder::class);
-        // \App\Models\Product::factory(500)->create();
+        // $this->call(ProductsTableSeeder::class);
+        \App\Models\Product::factory(500)->create();
 
-        $this->call(FAQsTableSeeder::class);
+        // $this->call(FAQsTableSeeder::class);
         \App\Models\FAQ::factory(20)->create();
 
         // $this->call(CartsTableSeeder::class);
-        // \App\Models\Cart::factory(100)->create();
+        \App\Models\Cart::factory(100)->create();
 
-        // \App\Models\Detail::factory(150)->create();
+        \App\Models\Detail::factory(150)->create();
         // $this->call(DetailsTableSeeder::class);
 
-        // \App\Models\Transaction::factory(50)->create();
-        // \App\Models\Transaction::factory(500)->create();
         // $this->call(TransactionsTableSeeder::class);
+        // \App\Models\Transaction::factory(50)->create();
+        \App\Models\Transaction::factory(500)->create();
     }
 }

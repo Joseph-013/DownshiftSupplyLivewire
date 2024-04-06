5ca4b4c36bb4c1f030e71ef013d51c3e21e77f07
joseph_013 <103237286+Joseph-013@users.noreply.github.com>
2024-04-07 01:02:56 +0800

    Update FAQsTableSeeder.php

--- database/seeders/FAQsTableSeeder.php
+++ database/seeders/FAQsTableSeeder.php
@@ -16,7 +16,7 @@ class FAQsTableSeeder extends Seeder
         DB::table('faqs')->insert([
             [
                 'question' => 'Are items refundable?',
-                'answer' => 'Items are not refundable. In a case where an item is defective, the item is to be shipped back to the shop by Downshift Supply. If an item is proven defective, it will be replaced and shipped to the customer at Downshift Supply’s expense. If the item is proven not defective, it will be returned to the customer at the customer\'s expense.',
+                'answer' => '"Items are non-refundable. In instances where an item is found to be defective, Downshift Supply will handle the return shipping. Should the item be confirmed as defective, a replacement will be sent to the customer at no additional charge by Downshift Supply. Conversely, if the item is not defective, it will be sent back to the customer, who will bear the shipping costs. The item will be returned using the delivery method initially selected during the purchase. If you wish to alter this service (either to delivery or pick-up) for the return of the item, please reach out directly to the owner. Furthermore, it is essential to email the owner when sending back the item or for any communications pertaining to item returns."',
             ],
             [
                 'question' => 'Do you accept gcash payment?',
@@ -29,6 +29,10 @@ class FAQsTableSeeder extends Seeder
             [
                 'question' => 'Can I pick-up my item at your shop?',
                 'answer' => 'Yes. We offer pick-up method for purchased items.',
+            ],
+            [
+                'question' => 'Where is your shop located?',
+                'answer' => 'Our shop is located at 140 Cordillera Street, Santa Mesa Heights, Quezon City.',
             ],
             [
                 'question' => 'Where is your shop located?',

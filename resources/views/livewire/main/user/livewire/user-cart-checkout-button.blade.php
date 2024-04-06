27ed9d5a879ed4b24739a04a48ba2310f9036b15
jamessuuu <jameslorenzsantos@gmail.com>
2024-04-06 14:28:51 +0800

    cart checkout button size for smaller screen fixed

--- resources/views/livewire/main/user/livewire/user-cart-checkout-button.blade.php
+++ resources/views/livewire/main/user/livewire/user-cart-checkout-button.blade.php
@@ -1,14 +1,14 @@
 <div class="w-full max-w-96 flex flex-col items-center justify-center">
     @if(!$emptyCart)
-    <button wire:click="checkout" id="checkoutButton"
-        class="sm:block h-10 md:w-72 flex flex-row items-center justify-center rounded-lg  bg-orange-500 ml-3 border-1 border-black text-white text-sm font-semibold text-spacing">
-        Check out
-    </button>
+        <button wire:click="checkout"
+                class="w-full sm:w-auto h-10 md:w-72 flex items-center justify-center rounded-lg bg-orange-500 border border-black text-white text-sm font-semibold text-spacing">
+            Check out
+        </button>
     @else
-    <button disabled id="checkoutButton"
-        class="sm:block h-10 md:w-72 flex flex-row items-center justify-center rounded-lg bg-gray-300 ml-3 border-1 border-gray-400 text-gray-600 text-sm font-semibold text-spacing cursor-not-allowed">
-        Check out
-    </button>
+        <button disabled
+                class="w-full sm:w-auto h-10 md:w-72 flex items-center justify-center rounded-lg bg-gray-300 border border-gray-400 text-gray-600 text-sm font-semibold text-spacing cursor-not-allowed">
+            Check out
+        </button>
     @endif
-    Additional fees may be added upon checkout.
+    <p class="text-xs text-gray-500 mt-1">Additional fees may be added upon checkout.</p>
 </div>

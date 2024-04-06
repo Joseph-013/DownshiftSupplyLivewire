d7597e319b2fadb7c81c63aec909868cda60cee2
jamessuuu <jameslorenzsantos@gmail.com>
2024-04-06 14:32:13 +0800

    Update cart.blade.php

--- resources/views/livewire/main/user/cart.blade.php
+++ resources/views/livewire/main/user/cart.blade.php
@@ -4,7 +4,7 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 flex flex-col h-full">
 
             <div class="w-full flex flex-row items-center justify-between mt-3 mb-4">
-                <h1 class="me-10 font-montserrat text-spacing font-semibold text-xl default-shadow text-orange-400 ">
+                <h1 class="lg:me-10 ml-5 font-montserrat text-spacing font-semibold text-xl default-shadow text-orange-400 ">
                     Shopping Cart
                 </h1>
             </div>

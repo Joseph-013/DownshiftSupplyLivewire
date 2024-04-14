f8370d47729f2fa9f4e7a7f455b7800dd5037802
joseph_013 <103237286+Joseph-013@users.noreply.github.com>
2024-04-14 22:28:58 +0800

    Update adminnav.blade.php

--- resources/views/livewire/layout/adminnav.blade.php
+++ resources/views/livewire/layout/adminnav.blade.php
@@ -36,7 +36,7 @@
             {{-- Badge --}}
             <div class="absolute inset-0 flex justify-end right-2 -top-2">
                 <div class="bg-red-600 text-white text-xs flex justify-center items-center h-fit rounded-full font-mono"
-                    style="padding: 1px 5px;">
+                    style="padding: 1px 4px 1px 6px;">
                     {{-- change me --}}
                     1
                 </div>

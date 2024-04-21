dcd3463840a9044913c0a3e9fc9a798ea118c909
Qiroo <valdecanasjustin@gmail.com>
2024-04-21 14:05:32 +0800

    notification responsive

--- resources/views/livewire/notification-badge-responsive.blade.php
+++ resources/views/livewire/notification-badge-responsive.blade.php
@@ -1,9 +1,16 @@
-<div>
-    @if($count != 0)
+<div class="flex flex-row">
+    @if($countCompleted != 0)
+    <div class="bg-green-600 text-white text-xs flex justify-center items-center h-fit rounded-full font-mono ml-3"
+        style="padding: 1px 6px;">
+        {{-- change me --}}
+        {{ $countCompleted > 99 ? '99+' : $countCompleted }}
+    </div>
+    @endif
+    @if($countProcessing != 0)
     <div class="bg-red-600 text-white text-xs flex justify-center items-center h-fit rounded-full font-mono ml-3"
         style="padding: 1px 6px;">
         {{-- change me --}}
-        {{ $count > 99 ? '99+' : $count }}
+        {{ $countProcessing > 99 ? '99+' : $countProcessing }}
     </div>
     @endif
 </div>

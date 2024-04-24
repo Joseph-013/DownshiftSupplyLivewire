fe536e3f68b37f6f94aa2e593d4755bfd1ecbd87
joseph_013 <103237286+Joseph-013@users.noreply.github.com>
2024-04-24 21:16:58 +0800

    Update delete-user-form.blade.php

--- resources/views/livewire/profile/delete-user-form.blade.php
+++ resources/views/livewire/profile/delete-user-form.blade.php
@@ -4,6 +4,7 @@ use App\Livewire\Actions\Logout;
 use Illuminate\Support\Facades\Auth;
 use Livewire\Volt\Component;
 use App\Models\User;
+use App\Models\Cart;
 
 new class extends Component {
     public string $password = '';

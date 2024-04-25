17ff52189283ebe6d3964cda4f3a9d2e9c07a41f
joseph_013 <103237286+Joseph-013@users.noreply.github.com>
2024-04-25 19:09:17 +0800

    images updated

--- database/seeders/ProductImagesSeeder.php
+++ database/seeders/ProductImagesSeeder.php
@@ -16,45 +16,153 @@ class ProductImagesSeeder extends Seeder
         DB::table('product_images')->insert([
             [
                 'product_id' => 1,
-                'image' => 'https://lh3.googleusercontent.com/drive-viewer/AKGpihag-wAvZDMfbayIpM2vaIbYBbCArkXGfeU1CjGs-mwmLRRAOT_OFedICwry_3nWFpBTcxmxRcg5-Um5RQToVXFQo2_5Vw=s1600-v0',
-            ],
-            [
-                'product_id' => 2,
-                'image' => 'https://lh3.googleusercontent.com/drive-viewer/AKGpihZUJACLJoqR1u5SCZNmu_uY19YH-aO43dNttK5CDBqIu4Oy8QcfDp0pYYdGcGB5r1iqkzrn9cOtfegZlgpup-Lq08ao8A=s1600-v0',
-            ],
-            [
-                'product_id' => 3,
-                'image' => 'https://lh3.googleusercontent.com/drive-viewer/AKGpihYzh3OLS4O4aCfFF1QC98w7UgNcKNDLaaVB0lK1FkfUun6r9jzQPAmwe3EeJBILN9yTKqxyXJZperNjSq9VuYJIf2pDoA=s1600-v0',
-            ],
-            [
-                'product_id' => 4,
-                'image' => 'https://lh3.googleusercontent.com/drive-viewer/AKGpihZc5aQa_nhGvQ19fq9yTvuNgn5VtwDaByJearOabobUvY1p3sLQZbhkKqJqs5C4mAQOB9MH4Q6Yi4Ne04QlrlZl7XuSeg=s1600-v0',
-            ],
-            [
-                'product_id' => 5,
-                'image' => 'https://lh3.googleusercontent.com/drive-viewer/AKGpihaFMzM_TJ5gFmPkyyPYCZeDRsAICOD1OphY8Fs_zRr0SVzhp27xvSFCrCveAsFgYLW-fl-Uis6BqQqODDmg-GDAqR5xvQ=s1600-v0',
-            ],
-            [
-                'product_id' => 6,
-                'image' => 'https://lh3.googleusercontent.com/drive-viewer/AKGpihYkXnUwM_6incB0OfUcgsksG0X17QES95ZTzHFdtevt6iBfoJqJ4TQZaEXt7EvgNnPnYpH5VectE1zkf1Wm9T_fDpx2=s1600-v0',
-            ],
-            [
-                'product_id' => 7,
-                'image' => 'https://lh3.googleusercontent.com/drive-viewer/AKGpihZIfb19ABkKGiTttfJdzHkpcqkYw2EEVh8GyQpSLwQsLnW0FmH9vCA4GIsNiyX6rIjxQUBDO8Mw4ONeEqrZvBwctTXSGYtS4kk=s1600-rw-v1',
-            ],
-            [
-                'product_id' => 8,
-                'image' => 'https://lh3.googleusercontent.com/drive-viewer/AKGpihZUJACLJoqR1u5SCZNmu_uY19YH-aO43dNttK5CDBqIu4Oy8QcfDp0pYYdGcGB5r1iqkzrn9cOtfegZlgpup-Lq08ao8A=s1600-v0',
-            ],
-            [
-                'product_id' => 9,
-                'image' => 'https://lh3.googleusercontent.com/drive-viewer/AKGpihaCZ2qesEGDgXB0kEr2Eln8F1KON7zG2yR5Zc-g3xjZEW9_8D6Ym4fBJP64cciQXtD3MVww6-u1mSIAY3HrYU4E1U23WQ=s1600-v0',
-            ],
-            [
-                'product_id' => 10,
-                'image' => 'https://lh3.googleusercontent.com/drive-viewer/AKGpihY_IM-AZ83Adadb2RbRhSvV8Asg-e9kf1f9bh_5iZA9kfHoaGu-N4rLLt77Pd09PpEYYykFdYxsA-pfC-ti8pm9btGJ=s1600-v0',
+                'image' => 'https://lh3.googleusercontent.com/drive-viewer/AKGpihacz-7PIKRBLu59YW_jQz6LHte5DCv83PC9Hwnt6pw_FkkE8Z_zTa-vSRAjF0F6Sbkf4Mmxe3RoW4aOP0mqyabL6w9OXg9cVDk=s1600-rw-v1',
             ],
 
+            [
+                'product_id' => 1,
+                'image' => 'https://lh3.googleusercontent.com/drive-viewer/AKGpihYCzKPSJsBNuQAv_D2etiU67Vq8taKR1Ci3pJWS_mm1nQ2Q0iddIskD87hvRv7tIr3VHzDrdYqoRUXOIX5DNg7YDe9s73QLRg=s1600-rw-v1',
+            ],
+
+            [
+                'product_id' => 1,
+                'image' => 'https://lh3.googleusercontent.com/drive-viewer/AKGpihZ6-UsTWGPhcv8Y3ntUhURi2TnJi2dFnhvT6VEGRZ-4b1AdcWGXDH32CumMI1ggAOLGaHBrIw9JCvJmmwJSqxm6gZ3GIlFD9Y4=s1600-rw-v1',
+            ],
+
+            [
+                'product_id' => 2,
+                'image' => 'https://lh3.googleusercontent.com/drive-viewer/AKGpihag75SE7Fw8qj6bqk7U0vs6vnjWfWYN4qvz0pP0vWx9tABSqDXFdWHzm1XZktjgoBVNy-FL4HWyVf0tysXNPPC_daHLth0_SaM=s1600-rw-v1',
+            ],
+
+            [
+                'product_id' => 2,
+                'image' => 'https://lh3.googleusercontent.com/drive-viewer/AKGpihZi5W7NW0QNv8iyxMVCBkVkvWawEc9EDcWsJ7oZawSMAfrIbGlx4V4KiAWq0_g1LhAWmPzpeW5BbzahEd1z3QJrDPCDONuQlPE=s1600-rw-v1',
+            ],
+
+            [
+                'product_id' => 2,
+                'image' => 'https://lh3.googleusercontent.com/drive-viewer/AKGpihbs1MZ4scP87TKorls5C-W03zqJtop-Kn2nJftcKlZV4L0iEax44EPh844LOwyJpu3LjAf-jVBN8qdxzu-z0usQEwwFHGxr7w=s1600-rw-v1',
+            ],
+
+            [
+                'product_id' => 3,
+                'image' => 'https://lh3.googleusercontent.com/drive-viewer/AKGpihZ2stKDtXU_-LzwlQ057rJOuYiJpjoABc1DC9KqMzLjrTgPfubzq_Ame5jNG648l4ZfZ9slC3iX8xBofC7isJKjt2B2iQI-kA=s1600-rw-v1',
+            ],
+
+            [
+                'product_id' => 3,
+                'image' => 'https://lh3.googleusercontent.com/drive-viewer/AKGpihZJqmjWj1dWs44FpIXE6swEFpfSVdsnd83hxH8TuX2oummVDB98ZmXn0NrBph0HgZF2zjC3IZil6aKUC7o3aaLHWGtLg2nmrA=s2560',
+            ],
+
+            [
+                'product_id' => 3,
+                'image' => 'https://lh3.googleusercontent.com/drive-viewer/AKGpihZp234t_7JlC-FZtQnpl452PcBD4EHCUmReXv8Oc8fkLvKteK_ZKXQE1T0soh2akHFpmSNOEGi8vQOneeSp3sxja5g3ekDfPw=s1600-rw-v1',
+            ],
+
+            [
+                'product_id' => 4,
+                'image' => 'https://lh3.googleusercontent.com/drive-viewer/AKGpihbubsFsXJlPeyXw40NchmvZeLYcJZ5hM4dnMQlsS_Vfz_HD8k1XUZDWHbSCughvSsFNGYt0_HfYZO7EVN2ljUNHHH9Wws5wxA0=s1600-rw-v1',
+            ],
+
+            [
+                'product_id' => 4,
+                'image' => 'https://lh3.googleusercontent.com/drive-viewer/AKGpihbubsFsXJlPeyXw40NchmvZeLYcJZ5hM4dnMQlsS_Vfz_HD8k1XUZDWHbSCughvSsFNGYt0_HfYZO7EVN2ljUNHHH9Wws5wxA0=s1600-rw-v1',
+            ],
+
+            [
+                'product_id' => 4,
+                'image' => 'https://lh3.googleusercontent.com/drive-viewer/AKGpihYAcur-80SRQnuGV2_y5kkKx5YTrq3pGbROXYHSfEcs8r8tXfgD0djidoIvD4y0rZrTbnToZyWkFz3g1uKongs1NvX0E66Qgg=s1600-rw-v1',
+            ],
+
+            [
+                'product_id' => 5,
+                'image' => 'https://lh3.googleusercontent.com/drive-viewer/AKGpihaxl5rnmsvGDeI2gnarQmLsnTzCXJOJyxpmgg8s9yR02RWWIAR3xPGlgXYZR62daYRvB66njqkROdePJcS1080gNhw9wYLhxA=s1600-rw-v1',
+            ],
+
+            [
+                'product_id' => 5,
+                'image' => 'https://lh3.googleusercontent.com/drive-viewer/AKGpihbosiinRGDL4wVpPmASIM_eye2-SpqF2RKc_waI7w366eca2d8PfJszaV9Up40Rh3kBM_2FJl_7GShTfOKYS-EOUrxcCzXRxGk=s1600-rw-v1',
+            ],
+
+            [
+                'product_id' => 5,
+                'image' => 'https://lh3.googleusercontent.com/drive-viewer/AKGpihY4_Sf2K1B9MtwEeelU8q7AOGh86ctPvNwFojLjgdHg0I-p7nJLKsBkCnoa1glJt4Dr3oSl9q3J4DC8Im0stF2_uGBjEeUJpPs=s1600-rw-v1',
+            ],
+
+            [
+                'product_id' => 6,
+                'image' => 'https://lh3.googleusercontent.com/drive-viewer/AKGpihbsmtubwB-hPfPDaZZKKzZk-TW4ml86iya-6KI0ZRWD6h28Zk5BkAF_SAvJ8621yT1HM2hUSHYyj7_BbWxPFQFluLrW0JeXIA=s1600-rw-v1',
+            ],
+
+            [
+                'product_id' => 6,
+                'image' => 'https://lh3.googleusercontent.com/drive-viewer/AKGpiha2Drj2pU-A7ho3M5aSA95us7-LlrYeqZf1eYpv_cJrnzU3iq-lR0WHmqcqQukwJzDSaDEjYWgIHYUPYQ6iESdiy6I7Roq4FSU=s1600-rw-v1',
+            ],
+
+            [
+                'product_id' => 6,
+                'image' => 'https://lh3.googleusercontent.com/drive-viewer/AKGpihbz_FZ7d0CQHCs3bqjIEFbW27niSu_YNG57ORMdcSyc9UTAXlrKXa8PjncCuPFhw7yOK51n-rgD8R6w3MPFUq7f-dRYoj9okNo=s1600-rw-v1',
+            ],
+
+            [
+                'product_id' => 7,
+                'image' => 'https://lh3.googleusercontent.com/drive-viewer/AKGpihagF82qzjgyJgeHwTMCqQwBWnAQhPgqBK8z4mK4tYd90KlXaoau0cYzWzSQ6mzlGMLT_WhVhVr3x5f9DlofxQ7LWMhx6EnXEEE=s1600-rw-v1',
+            ],
+
+            [
+                'product_id' => 7,
+                'image' => 'https://lh3.googleusercontent.com/drive-viewer/AKGpihYXcwUuW_LQRU1Dxn14ncnatLt-ypc5WyZyLL0dOnBtUCGGPf7Es6-1O5-oBYWgrug6PMiDKSfwTYDWruRvFDnNSDSOSmU1kNM=s1600-rw-v1',
+            ],
+
+            [
+                'product_id' => 7,
+                'image' => 'https://lh3.googleusercontent.com/drive-viewer/AKGpihZDO0FMwR82wVxPM4wPSjP7BNayPu9NicBg6aqiVDgA4w3yu14wzHiX7wE47bBrh8dFpwZxP90Rw5lb_1m_WbUWHJK70GZG1Q=s1600-rw-v1',
+            ],
+
+            [
+                'product_id' => 8,
+                'image' => 'https://lh3.googleusercontent.com/drive-viewer/AKGpihZYSPGtQAUa0NBdCIHLKyQyWKll52dz9dslQIwMHlh6wTIQaOBaaEJCSHJF1jgMPHguRQH7f-h2Miah67rVYtpLplL5dxUN8HU=s1600-rw-v1',
+            ],
+
+            [
+                'product_id' => 8,
+                'image' => 'https://lh3.googleusercontent.com/drive-viewer/AKGpihaVsg_JtMQHe-qbS5SlKhkOsKKfedaBmtNNKhLirf2DnIYJd1HyX1ROha-n4LltDGISF3W2H9d2AlXryr0Fa1Vs9DoSo7vQNDY=s1600-rw-v1',
+            ],
+
+            [
+                'product_id' => 8,
+                'image' => 'https://lh3.googleusercontent.com/drive-viewer/AKGpihauR-gMCy-F8gK2mKvQBJk5GmfKW58EerH4u6UW-KZY01GqnC51uwdcz0-ZQWl496G0nZulcYwlA8Zjfx4CbJJPw0MoGI-1TnM=s1600-rw-v1',
+            ],
+
+            [
+                'product_id' => 9,
+                'image' => 'https://lh3.googleusercontent.com/drive-viewer/AKGpihbKc6tvWtpq3iVQebVESG6UR3nv2_Y0FFj2fAPS1cAsfFxF6yNw1hIQpGMU4tlFKopVS97rxEQzmsEgPidAsH8L3UqXXWhYPg=s1600-rw-v1',
+            ],
+
+            [
+                'product_id' => 9,
+                'image' => 'https://lh3.googleusercontent.com/drive-viewer/AKGpihZb0U3TsElHJDoPtuaGsTeVRdgZCGjQHd2zRAvrXFxpRCAYqq0yQBSkgcyPqeeCnps2eS5ypLa36rWj1u_nQ0ke_jYxeMJeTQ=s1600-rw-v1',
+            ],
+
+            [
+                'product_id' => 9,
+                'image' => 'https://lh3.googleusercontent.com/drive-viewer/AKGpihbnk3xeYlZJ9X-QLJqKpl0Tu6OC6K7czg6iCJ3qDRpS-adWktuEMOqQk_BSWMpcD00FDyb0O_5tvnjkgtHe4jRDEepMLd6HYzU=s1600-rw-v1',
+            ],
+
+            [
+                'product_id' => 10,
+                'image' => 'https://lh3.googleusercontent.com/drive-viewer/AKGpihYmMWEHihGJvwQg7SU3PSQWjz9oOkrkFh6PqFCmBT1RJQOzGseTdkVB9oGiKLgF6uxTs4NEJnXQ-QUQxqwKqzMCpICM4OnWtg=s1600-rw-v1',
+            ],
+
+            [
+                'product_id' => 10,
+                'image' => 'https://lh3.googleusercontent.com/drive-viewer/AKGpihbQFydNkbwQHLpPXLrK4EplaLEzgWsZ4ljoeKWGAiiCq03pNic5Jvqe1glsXYStOJG-6FCjB8vLsuSEVT7kMIAktEHwYME8M8c=s1600-rw-v1',
+            ],
+
+            [
+                'product_id' => 10,
+                'image' => 'https://lh3.googleusercontent.com/drive-viewer/AKGpihZPSHi7PBTFyvdSjYGFgcmSTqGl8JLCvzrqQrwQcZkk1mOl3ZbzoAmw-Pr1BjqbI_JJrUftWv-oFFah9FP3CwzCLJL0yALVRQ=s1600-rw-v1',
+            ],
         ]);
     }
 }

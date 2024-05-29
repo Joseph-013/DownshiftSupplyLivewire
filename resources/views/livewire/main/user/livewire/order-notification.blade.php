ef59c302e311629bdb57333d601fc5bcbc230f4e
joseph_013 <103237286+Joseph-013@users.noreply.github.com>
2024-05-29 21:04:25 +0800

    Update order-notification.blade.php

--- resources/views/livewire/main/user/livewire/order-notification.blade.php
+++ resources/views/livewire/main/user/livewire/order-notification.blade.php
@@ -6,8 +6,8 @@
                 <h2 class="text-xl mb-4 text-center">Your order '{{ $transaction->id }}' is on its way!</h2>
                 <p class="text-sm text-gray-600 mb-4 text-center">Have you received order '{{ $transaction->id }}' yet?
                 </p>
-                <p class="text-sm text-gray-600 mb-4 text-center">Once you choose yes, the order is set to complete. This
-                    is an irreversible action.</p>
+                <p class="text-sm text-gray-600 mb-4 text-center">Once you choose yes, the order is permanently set to
+                    complete.</p>
                 <div class="flex justify-center space-x-4">
                     <button wire:click="notYet()"
                         class="flex-none w-24 py-2 text-sm bg-red-600 text-white border border-red-300 rounded hover:bg-red-700">Not
